713bb87c667aa6e1206a7a56121ebefd2d26c920
Slightlyxz <80231852+Slightlyxz@users.noreply.github.com>
2025-06-14 12:27:27 +0200

    ConfigWindow changes

--- Snappy/Windows/ConfigWindow.cs
+++ Snappy/Windows/ConfigWindow.cs
@@ -1,5 +1,6 @@
 using Dalamud.Interface;
 using Dalamud.Interface.ImGuiFileDialog;
+using Dalamud.Interface.Utility;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 using System;
@@ -14,13 +15,12 @@ public class ConfigWindow : Window, IDisposable
     private FileDialogManager FileDialogManager;
     public string Version { get; set; } = string.Empty;
 
-    //ImGuiWindowFlags.NoResize
     public ConfigWindow(Plugin plugin) : base(
         "Snappy Settings",
          ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
         ImGuiWindowFlags.NoScrollWithMouse | ImGuiWindowFlags.NoResize)
     {
-        this.Size = new Vector2(500, 115);
+        this.Size = new Vector2(350, 120) * ImGuiHelpers.GlobalScale;
         this.SizeCondition = ImGuiCond.Always;
 
         this.Configuration = plugin.Configuration;
@@ -65,7 +65,14 @@ public class ConfigWindow : Window, IDisposable
         // Add version label at the bottom
         ImGui.Spacing();
         ImGui.Separator();
-        ImGui.Text($"Snappy version {typeof(Plugin).Assembly.GetName().Version?.ToString() ?? "Unknown"}. Made with <3 .");
+
+        // Display version text with red heart
+        ImGui.Text($"Snappy version {typeof(Plugin).Assembly.GetName().Version?.ToString() ?? "Unknown"}. Made with");
+        ImGui.SameLine();
+        ImGui.PushFont(UiBuilder.IconFont);
+        ImGui.PushStyleColor(ImGuiCol.Text, new Vector4(0.82f, 0.18f, 0.18f, 1.0f)); // Red color (similar to 0xFF3030D0)
+        ImGui.Text(FontAwesomeIcon.Heart.ToIconString());
+        ImGui.PopStyleColor();
+        ImGui.PopFont();
     }
 }
-
